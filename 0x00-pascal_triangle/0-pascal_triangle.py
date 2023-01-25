24f74cd405131eed74f3c554510529fb03ef92f1
crownpoz <agbeyoadebimpe@gmail.com>
2023-01-25 22:22:38 +0100

    pascal

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,22 +1,24 @@
 #!/usr/bin/python3
-"""Pascal triangle"""
+'''
+    Pascal's Triangle.
+'''
 
 
 def pascal_triangle(n):
-    """returns a list of lists of integers 
-    representing the Pascal’s triangle of n:
-    if n <= 0 Returns an empty list"""
-
+    '''
+        Function to draw Pascal's Triangle.
+    '''
     if n <= 0:
-        return []
-
-    output = [[1]]
-
-    for i in range(n - 1):
-        temp = [0] + output[-1] + [0]
-        row = []
-        for j in range(len(output[-1]) + 1):
-            row.append(temp[j] + temp[j + 1])
-            output.append(row)
-
-    return (output)
+        return ([])
+    if n == 1:
+        return [[1]]
+    if n == 2:
+        return [[1], [1, 1]]
+    triangle = [[1], [1, 1]]
+    for i in range(2, n):
+        row = [1]
+        for j in range(1, i):
+            row.append(triangle[i-1][j-1] + triangle[i-1][j])
+        row.append(1)
+        triangle.append(row)
+    return triangle
